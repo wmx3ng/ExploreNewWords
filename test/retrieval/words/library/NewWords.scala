d38d3702aeb1d1c819c0019fdb09415219251af4
Arvin Wong <wmx3ng@163.com>
2015-12-21 23:41:04 +0800

    规划生成词的结构

--- test/retrieval/words/library/NewWords.scala
+++ test/retrieval/words/library/NewWords.scala
@@ -1,8 +1,12 @@
 package retrieval.words.library
 
+import scala.collection.mutable.HashMap
+
 /**
- *产生的新词；
- *  */
+ * 产生的新词；
+ */
 class NewWords {
+  //
+  private var terms = HashMap[String, Int]()
 
 }
