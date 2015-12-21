c6fe911b24579869c3c2dd941a5fd354573282d3
Wang Mingxing <wmx@deepdata.com.cn>
2015-12-21 14:10:01 +0800

    获取词项
    用IK分词器分词

--- test/retrieval/words/ObtainHighLighter.scala
+++ test/retrieval/words/ObtainHighLighter.scala
@@ -9,39 +9,42 @@ import org.apache.lucene.util.Version
 import org.wltea.analyzer.lucene.IKAnalyzer
 import org.apache.lucene.queryparser.classic.QueryParser;
 
-object ObtainTerms {
-  def highLighter(text: String, queryString: String) = {
+/**
+ *
+ */
+object ObtainHighLighter {
+  def highLighter( text : String, queryString : String ) = {
     val analyzer = new StandardAnalyzer()
-    val tokenStreams = analyzer.tokenStream("title", new StringReader(text))
-    val parser = new QueryParser("title", new StandardAnalyzer())
-    val query = parser.parse(queryString)
-    val simpleHTMLFormatter = new SimpleHTMLFormatter("<font color='red'>", "</font>")
-    val scorer = new QueryScorer(query)
-    val hl = new Highlighter(simpleHTMLFormatter, scorer)
+    val tokenStreams = analyzer.tokenStream( "title", new StringReader( text ) )
+    val parser = new QueryParser( "title", new StandardAnalyzer() )
+    val query = parser.parse( queryString )
+
+    val simpleHTMLFormatter = new SimpleHTMLFormatter( "<font color='red'>", "</font>" )
+    val scorer = new QueryScorer( query )
+    val hl = new Highlighter( simpleHTMLFormatter, scorer )
     //    hl.getBestFragment(tokenStreams, text)
-    hl.getBestFragments(tokenStreams, text, 3, "...")
+    hl.getBestFragments( tokenStreams, text, 3, "..." )
   }
 
-  def highLighter_ik(text: String, queryString: String) = {
+  def highLighter_ik( text : String, queryString : String ) = {
     val analyzer = new IKAnalyzer
 
-    val tokenStreams = analyzer.tokenStream("title", new StringReader(text))
+    val tokenStreams = analyzer.tokenStream( "title", new StringReader( text ) )
 
-    val parser = new QueryParser("title", new IKAnalyzer)
-    val query = parser.parse(queryString)
-    val simpleHTMLFormatter = new SimpleHTMLFormatter("<font color='red'>", "</font>")
-    val scorer = new QueryScorer(query)
-    val hl = new Highlighter(simpleHTMLFormatter, scorer)
+    val parser = new QueryParser( "title", new IKAnalyzer )
+    val query = parser.parse( queryString )
+    val simpleHTMLFormatter = new SimpleHTMLFormatter( "<font color='red'>", "</font>" )
+    val scorer = new QueryScorer( query )
+    val hl = new Highlighter( simpleHTMLFormatter, scorer )
     //    hl.getBestFragment(tokenStreams, text)
-    hl.getBestFragments(tokenStreams, text, 3, "...")
+    hl.getBestFragments( tokenStreams, text, 3, "..." )
   }
 
-  def main(args: Array[String]) {
+  def main( args : Array[String] ) {
     val text = "北京安监部门：清华大学实验室爆炸事故在使用氢气做化学实验中发生北京安监部门：清华大学实验室爆炸事故在使用氢气做化学实验中发生北京安监部门：清华大学实验室爆炸事故在使用氢气做化学实验中发生北京安监部门：清华大学实验室爆炸事故在使用氢气做化学实验中发生北京安监部门：清华大学实验室爆炸事故在使用氢气做化学实验中发生北京安监部门：清华大学实验室爆炸事故在使用氢气做化学实验中发生北京安监部门：清华大学实验室爆炸事故在使用氢气做化学实验中发生北京安监部门：清华大学实验室爆炸事故在使用氢气做化学实验中发生"
     val queryString = "清华大学"
-    println(highLighter(text, queryString))
-    println("------------------------------------")
-    println(highLighter_ik(text, queryString))
-
+    println( highLighter( text, queryString ) )
+    println( "------------------------------------" )
+    println( highLighter_ik( text, queryString ) )
   }
 }
--- test/retrieval/words/ObtainTerms.scala
+++ test/retrieval/words/ObtainTerms.scala
@@ -6,31 +6,38 @@ import java.io.StringReader
 import org.apache.lucene.analysis.standard.StandardAnalyzer
 import org.apache.lucene.util.Version
 import org.apache.lucene.analysis.tokenattributes.OffsetAttribute
-
 import org.apache.lucene.search.highlight.Highlighter
 import org.apache.lucene.search.highlight.SimpleHTMLFormatter
 import org.apache.lucene.search.highlight.QueryScorer
-import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.queryparser.classic.QueryParser
+import org.apache.lucene.analysis.tokenattributes.OffsetAttributeImpl
+import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttributeImpl
+import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute
+import org.apache.lucene.analysis.tokenattributes.CharTermAttributeImpl
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute
+import org.apache.lucene.analysis.tokenattributes.TypeAttribute
 
 /**
- * 高亮测试
+ * 获取分词词项;
  */
 object HL {
-  def highLighter(text: String, queryString: String) = {
-    val analyzer = new StandardAnalyzer()
-    val tokenStreams = analyzer.tokenStream("title", new StringReader(text))
-    val parser = new QueryParser("title", new StandardAnalyzer())
-    val query = parser.parse(queryString)
-    val simpleHTMLFormatter = new SimpleHTMLFormatter("<font color='red'>", "</font>")
-    val scorer = new QueryScorer(query)
-    val hl = new Highlighter(simpleHTMLFormatter, scorer)
-    hl.getBestFragment(tokenStreams, text)
+  def obtainTerms( text : String ) {
+    val analyzer = new IKAnalyzer
+    val tokenStreams = analyzer.tokenStream( "title", new StringReader( text ) )
+    tokenStreams.reset()
+
+    val termAttr = tokenStreams.addAttribute( classOf[CharTermAttribute] )
+    val offset = tokenStreams.addAttribute( classOf[OffsetAttribute] )
+    val pos = tokenStreams.addAttribute( classOf[PositionIncrementAttribute] )
+    val termType = tokenStreams.addAttribute( classOf[TypeAttribute] )
+
+    while ( tokenStreams.incrementToken() ) {
+      println( "positionIncre:" + pos.getPositionIncrement + " type:" + termType + " term:" + termAttr + " offset_begin:" + offset.startOffset() + " offset_end:" + offset.endOffset() )
+    }
   }
 
-  def main(args: Array[String]) {
-    val text = "北京安监部门：清华大学实验室爆炸事故在使用氢气做化学实验中发生"
-    val queryString = "清华大学"
-    println(highLighter(text, queryString))
-
+  def main( args : Array[String] ) {
+    val text = "现在社会上有很多坑爹的货"
+    obtainTerms( text )
   }
 }
