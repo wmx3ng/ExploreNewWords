ed655777057de67f3ff7c578df0355e8cbc1cbf4
Wang Mingxing <wmx@deepdata.com.cn>
2016-01-23 13:37:31 +0800

    初个版本

--- src/retrieval/analysis/test/ansj/TestAnsj.scala
+++ src/retrieval/analysis/test/ansj/TestAnsj.scala
@@ -2,10 +2,12 @@ package retrieval.analysis.test.ansj
 
 import org.ansj.splitWord.analysis.ToAnalysis
 
-object TestAnsj extends App {
-  val text = "北京雾霾北京华悦科技有限公司中国武林风"
-  val terms = ToAnalysis.parse(text)
-  for (i <- 0 until terms.size()) {
-    println(terms.get(i).getName + " " + terms.get(i).getNatureStr + " " + terms.get(i).score())
+object TestAnsj {
+  def main(args : Array[String]) {
+    val text = "北京雾霾北京华悦科技有限公司中国武林风"
+    val terms = ToAnalysis.parse(text)
+    for (i <- 0 until terms.size()) {
+      println(terms.get(i).getName + " " + terms.get(i).getNatureStr + " " + terms.get(i).score())
+    }
   }
 }
